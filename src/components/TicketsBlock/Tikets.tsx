ff316ab19d1361358e02943bfb2f88eda7a02552
Oleg <Kornyakovo@gmail.com>
2022-07-28 17:09:09 +0400

    feat: add From/To action

--- src/components/TicketsBlock/Tikets.tsx
+++ src/components/TicketsBlock/Tikets.tsx
@@ -1,42 +1,83 @@
 import { useEffect } from 'react';
 import { useAppSelector, useAppDispatch } from '../../hooks'
 import { GetTikets } from '../../store/actions/TiketsAction';
-import TiketsAmount, { TiketsUpdate } from '../../store/actions/TiketsAmount';
-import { TiketsState } from '../../store/actions/TiketsAction';
-import { fromWhereUpdate } from '../../store/actions/TicketSettingsAction';
 
-function fromUpdate(store: TiketsState, from: string) {
-    let inf = store.value.slice()
+type ticketsSlicedType = {
+    companyId: string;
+    id: string;
+    info: {
+        dateEnd: number;
+        dateStart: number;
+        destination: string;
+        duration: number;
+        origin: string;
+        stops: string[];
+    };
+    price: number;
+}[]
+
+function fromUpdate(store: ticketsSlicedType, city: string, fromTo: string) {
+    let inf = store.slice()
     let result = []
     for (let i = 0; i < inf.length; i++) {
-        if (inf[i].info.origin === from.toUpperCase())
-            result.push(inf[i])
+        if (fromTo === 'from') {
+            if (inf[i].info.origin === city.toUpperCase())
+                result.push(inf[i])
+        } else {
+            if (inf[i].info.destination === city.toUpperCase()) {
+                result.push(inf[i])
+            }
+        }
     }
     if (result.length > 0)
         return result
     else
-    return inf
-}
-function byPrice(a: any, b: any) {
-    console.log(typeof a)
-    return a.price - b.price
+        return inf
 }
 
 function Tiket(Component: { dataId: number }) {
+    const dispatch = useAppDispatch()
+
     const status = useAppSelector(state => state.TiketsState.status)
     const tickets = useAppSelector(state => state.TiketsState)
     const inputFromWhere = useAppSelector(state => state.TiketsSettings.fromWhere)
+    const inputToWhere = useAppSelector(state => state.TiketsSettings.toWhere)
+    let temptickets: ticketsSlicedType
+    if (status === 'succes') {
+        temptickets = tickets.value.slice()
+    }
+
+    useEffect(() => {
+        if (status === 'succes') {
+            if (inputFromWhere.length === 3) {
+                temptickets = fromUpdate(temptickets, inputFromWhere, 'from')
+            }
+            if (inputToWhere.length === 3) {
+                temptickets = fromUpdate(temptickets, inputToWhere, 'to')
+            }
+            if (Component.dataId >= temptickets.length - 1) {
+                dispatch(TiketsErrorUpdate(temptickets.length))
+            }
+            dispatch(TiketsMaxValueUpdate(temptickets.length))
+        }
+    })
 
     if (status === 'loading') return (<div><h1>{status}</h1></div>)
     else if (status === 'failed') return (<div><h1>{status}</h1></div>)
     else if (status === 'succes') {
-        let temptickets = tickets.value.slice()
-        // const inputFromWhere = useAppSelector(state => state.TiketsSettings.fromWhere)
         if (inputFromWhere.length === 3) {
-            temptickets = fromUpdate(tickets, inputFromWhere)
+            temptickets = fromUpdate(temptickets, inputFromWhere, 'from')
+        }
+        if (inputToWhere.length === 3) {
+            temptickets = fromUpdate(temptickets, inputToWhere, 'to')
         }
 
-        // temptickets.sort(byPrice)
+        //--- tiketsAmount error fix
+        if (Component.dataId >= temptickets.length) {
+            return (<div></div>)
+        }
+        //---
+
         const ticketInfo = temptickets[Component.dataId]
         const dateStart = new Date(ticketInfo.info.dateStart)
         const dateEnd = new Date(ticketInfo.info.dateEnd)
@@ -76,16 +117,33 @@ function Tiket(Component: { dataId: number }) {
         </div>)
     }
 }
-function Tikets() {
-    const dispatch = useAppDispatch()
 
+function AddTiketsbutton() {
+    const dispatch = useAppDispatch()
+    const tiketsmaxAmount = useAppSelector(state => state.TiketsAmount.maxValue)
     const tiketsAmount = useAppSelector(state => state.TiketsAmount.value)
+    let maxAdd = 5
+    if (tiketsmaxAmount - tiketsAmount < 5) {
+        maxAdd = tiketsmaxAmount - tiketsAmount
+    }
+    return (
+        <button onClick={() => dispatch(TiketsUpdate())}>ПОКАЗАТЬ ЕЩЕ {maxAdd} БИЛЕТОВ</button>
+    )
+}
+import TiketsAmount, { TiketsUpdate, TiketsErrorUpdate, TiketsMaxValueUpdate } from '../../store/actions/TiketsAmount';
+
+
+
+export default function Tikets() {
+    const dispatch = useAppDispatch()
     useEffect(() => {
         dispatch(GetTikets())
     }, [dispatch])
+
+    const tiketsAmount = useAppSelector(state => state.TiketsAmount.value)
+
     return (<div>
         {([...Array(tiketsAmount)].map((e, i: number) => <Tiket key={i} dataId={i} />))}
-        <button onClick={() => dispatch(TiketsUpdate())}>ПОКАЗАТЬ ЕЩЕ 5 БИЛЕТОВ</button>
+        <AddTiketsbutton />
     </div>)
 }
-export default Tikets
--- src/store/actions/TiketsAmount.tsx
+++ src/store/actions/TiketsAmount.tsx
@@ -2,10 +2,14 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
 interface Tikets {
     value: number
+    maxValue: number
+    valueStatus: string
 }
 
 const initialState: Tikets = {
-    value: 6
+    value: 6,
+    maxValue: undefined,
+    valueStatus: 'notFull'
 }
 
 export const TiketsLogic = createSlice({
@@ -13,10 +17,29 @@ export const TiketsLogic = createSlice({
     initialState,
     reducers: {
         TiketsUpdate: (state) => {
-            state.value+=5
+            if ((state.value + 5) < state.maxValue) {
+                state.value += 5
+                state.valueStatus = 'notFull'
+            } else {
+                state.value = state.maxValue
+                state.valueStatus = 'full'
+            }
+        },
+        TiketsMaxValueUpdate: (state, action: PayloadAction<number>) => {
+            state.maxValue = action.payload
+            if (state.value >= state.maxValue) {
+                state.value = state.maxValue
+                state.valueStatus = 'full'
+            } else{
+                state.value = 6
+                state.valueStatus = 'notFull'
+            }
+        },
+        TiketsErrorUpdate: (state, action: PayloadAction<number>) => {
+            state.value = action.payload
         }
     }
 })
-export const { TiketsUpdate } = TiketsLogic.actions
+export const { TiketsUpdate, TiketsMaxValueUpdate, TiketsErrorUpdate } = TiketsLogic.actions
 
 export default TiketsLogic.reducer
